c469b65eff6f43dad7cf65544895869ee9519e8f
Darcy Price <darcyelsing@gmail.com>
2020-06-30 10:10:09 +1000

    cleaned up JSX

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,26 +32,28 @@ export default function Sidebar() {
   };
 
   return (
-    <div className='sidebar-wrapper'>
-      {visible ? (
-        <StyledToggle visible={visible} onClick={() => setVisible(!visible)}>
-          <AiOutlineArrowRight />
-        </StyledToggle>
-      ) : (
-        <StyledToggle visible={visible} onClick={() => setVisible(!visible)}>
-          <AiOutlineArrowLeft />
-        </StyledToggle>
-      )}
-      <StyledContainer visible={visible}>
+    <StyledWrapper visible={visible}>
+      <div className='toggle-wrapper'>
+        {visible ? (
+          <StyledToggle visible={visible} onClick={() => setVisible(!visible)}>
+            <AiOutlineArrowRight />
+          </StyledToggle>
+        ) : (
+          <StyledToggle visible={visible} onClick={() => setVisible(!visible)}>
+            <AiOutlineArrowLeft />
+          </StyledToggle>
+        )}
+      </div>
+      <div className='content'>
         <LocationButtons />
-        <StyledControls>
-          <div className='content'>
-            <Form form={form}>
-              <Form.Item>
-                <Button htmlType='reset' onClick={() => handleReset()}>
-                  RESET
-                </Button>
-              </Form.Item>
+        <div className='form-wrapper'>
+          <Form form={form}>
+            <div className='reset-wrapper'>
+              <Button htmlType='reset' onClick={() => handleReset()}>
+                RESET
+              </Button>
+            </div>
+            <div className='accordions-wrapper'>
               <Collapse expandIconPosition='right'>
                 <Panel header='Key Freight Routes'>
                   {keyFreightRouteSwitches.map((v) => (
@@ -74,13 +76,57 @@ export default function Sidebar() {
                   ))}
                 </Panel>
               </Collapse>
-            </Form>
-          </div>
-        </StyledControls>
-      </StyledContainer>
-    </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div`
+  .content {
+    padding: 30px;
+    background: #fff;
+    height: 100%;
+    position: absolute;
+    width: 350px;
+    height: 100vh;
+    z-index: 2;
+    right: 30px;
+    top: 0;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    visibility: ${(props) => (props.visible ? 'visible' : 'hidden')};
+    opacity: ${(props) => (props.visible ? '1' : '0')};
+    transition: visibility 0.5s, opacity 0.1s linear;
+  }
+
+  .content .form-wrapper {
+    margin-top: 50px;
+    overflow: scroll;
+    max-height: 70%;
+  }
+
+  .content .form-wrapper .accordions-wrapper {
+    margin-top: 30px;
+  }
+
+  .ant-collapse {
+    margin: 8px 0;
+
+    .ant-collapse-item {
+    }
+
+    .ant-collapse-item-active {
+      background: #1890ff;
+
+      .ant-collapse-header {
+        color: #fff;
+      }
+    }
+  }
+`;
 
 const StyledToggle = styled(Button)`
   position: absolute;
@@ -90,47 +136,4 @@ const StyledToggle = styled(Button)`
   height: 50px;
   background: #9f9ffd;
   border: none;
-`;
-
-const StyledContainer = styled.div`
-  padding: 30px;
-  background: #fff;
-  height: 100%;
-  position: absolute;
-  width: 350px;
-  height: 100vh;
-  z-index: 2;
-  right: 30px;
-  top: 0;
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-  visibility: ${(props) => (props.visible ? 'visible' : 'hidden')};
-  opacity: ${(props) => (props.visible ? '1' : '0')};
-  transition: visibility 0.5s, opacity 0.1s linear;
-`;
-
-const StyledControls = styled.div`
-  margin-top: 50px;
-  overflow: scroll;
-  max-height: 70%;
-
-  .content {
-    .ant-form .ant-form-item button {
-    }
-
-    .ant-collapse {
-      margin: 8px 0;
-
-      .ant-collapse-item {
-      }
-
-      .ant-collapse-item-active {
-        background: #1890ff;
-
-        .ant-collapse-header {
-          color: #fff;
-        }
-      }
-    }
-  }
 `;
