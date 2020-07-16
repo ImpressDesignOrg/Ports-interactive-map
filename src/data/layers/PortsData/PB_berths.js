ecd864cb4bddb5493b76df0931a0d60f75707e72
Darcy Price <darcyelsing@gmail.com>
2020-07-16 15:04:04 +1000

    added custom popup template content

--- src/data/layers/PortsData/PB_berths.js
+++ src/data/layers/PortsData/PB_berths.js
@@ -1,8 +1,10 @@
 import { berth } from "../../symbols";
 import { clusterConfig } from "../../../utils/cluster-config";
+import { handlePopupContent } from "../../../utils/popup-template";
 
 const PB_berthLayer = {
   url: "https://raw.githubusercontent.com/darcydev/StaticMedia/master/api/Ports/gatenumbers/json/PB_BERTH.geojson",
+  outFields: ["*"],
   objectIdField: "ObjectID",
   featureReduction: clusterConfig,
   renderer: {
@@ -16,125 +18,8 @@ const PB_berthLayer = {
   },
   popupTemplate: {
     title: "Port Botany {TextString}",
-    highlightEnabled: true,
-    content: [
-      {
-        type: "fields",
-        fieldInfos: [
-          {
-            fieldName: "BerthManager",
-            label: "Berth Manager",
-            visible: true,
-          },
-          {
-            fieldName: "Commodities",
-            label: "Commodities",
-            visible: true,
-          },
-          {
-            fieldName: "ChannelMaintainedDepth",
-            label: "Channel Maintained Depth",
-            visible: true,
-          },
-          {
-            fieldName: "DynamicUnderkeelClearanceSystemInUse",
-            label: "Dynamic Underkeel Clearance System in use",
-            visible: true,
-          },
-          {
-            fieldName: "StaticVesselDraftInChannel",
-            label: "Static Vessel Draft In Channel",
-            visible: true,
-          },
-          {
-            fieldName: "UKCRequirementsAtBerth",
-            label: "UKC Requirements At Berth",
-            visible: true,
-          },
-          {
-            fieldName: "DeclaredDepthAtBerth",
-            label: "Declared Depth At Berth",
-            visible: true,
-          },
-          {
-            fieldName: "IndicativeVesselDraftAtBerth",
-            label: "Indicative Vessel Draft At Berth",
-            visible: true,
-          },
-          {
-            fieldName: "MaxVesselLength",
-            label: "Max Vessel Length",
-            visible: true,
-          },
-          {
-            fieldName: "BerthBoxLength",
-            label: "Berth Box Length",
-            visible: true,
-          },
-          {
-            fieldName: "BerthBoxWidth",
-            label: "Berth Box Width",
-            visible: true,
-          },
-          {
-            fieldName: "WharfLevel",
-            label: "Wharf Level",
-            visible: true,
-          },
-          {
-            fieldName: "AirDraftLevel",
-            label: "Air Draft Level",
-            visible: true,
-          },
-          {
-            fieldName: "MaxVesselDeadweight",
-            label: "Max Vessel Deadweight",
-            visible: true,
-          },
-          {
-            fieldName: "MaxVesselDisplacementBerthingVelocity",
-            label: "Max Vessel Displacement Berthing Velocity",
-            visible: true,
-          },
-          {
-            fieldName: "MooringEquipment",
-            label: "Mooring Equipment",
-            visible: true,
-          },
-          {
-            fieldName: "TidalRange",
-            label: "Tidal Range",
-            visible: true,
-          },
-          {
-            fieldName: "RailAccess",
-            label: "Rail Access",
-            visible: true,
-          },
-
-          {
-            fieldName: "UnloadingEquipment",
-            label: "Unloading Equipment",
-            visible: true,
-          },
-          {
-            fieldName: "OperatingHours",
-            label: "Operating Hours",
-            visible: true,
-          },
-          {
-            fieldName: "AdditionalInformation",
-            label: "Additional Information",
-            visible: true,
-          },
-          {
-            fieldName: "Disclaimer",
-            label: "Disclaimer",
-            visible: true,
-          },
-        ],
-      },
-    ],
+    outfields: ["*"],
+    content: handlePopupContent,
   },
 };
 
---
+++ src/utils/popup-template.js
@@ -0,0 +1,30 @@
+/**
+ * A function to accept a marker feature and iterate over the object and return an
+ * html string of all the non-null metadata values in that object
+ * @param {object} feature
+ * @returns {string} html table
+ */
+export const handlePopupContent = (feature) => {
+  const dataObj = feature.graphic.attributes;
+
+  let htmlString = "";
+
+  console.log("dataObj :>> ", dataObj);
+
+  // iterate over the metadata object and add all non-null values to as a new table in a row
+  Object.keys(dataObj).forEach((key) => {
+    const value = dataObj[key];
+
+    // we don't need these keys in the popup
+    if (key === "Angle" || key === "ObjectID" || key === "TextString") {
+      // do nothing (not a placeholder; really do nothing!)
+    } else {
+      // the returned string only contains non-null values
+      if (value !== null) {
+        htmlString = htmlString + `<tr><td>${key}</td><td>${value}</td></tr>`;
+      }
+    }
+  });
+
+  return `<table style="width:100%" class='custom-popup-template-table'>${htmlString}</table>`;
+};
